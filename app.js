dd4eaf9bda46954b67a8e4d958fa3a5f4a174632
wilsonbotweyo <115102368+wilsonbotweyo@users.noreply.github.com>
2023-02-10 11:31:25 +0300

    second commit

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-const express = require('express');
-const app = express();
-
+var express = require('express');
+var app = express();
+// setting the middleware
 app.use(express.static('public'));
+// giving the port a number
+var port = process.env.PORT || 5030;
 
-app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
+var server = app.listen(port, () => {
+    console.log(`Server started on port `+ port);
 });
